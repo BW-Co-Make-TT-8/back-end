374522fbfe3895e052426c8a198b890a3dbd60a7
seth-bradshaw <seth.bradshaw27@gmail.com>
2021-01-03 20:10:42 -0700

    created SeedData

--- co-make-db/src/main/java/com/comake/server/models/Location.java
+++ co-make-db/src/main/java/com/comake/server/models/Location.java
@@ -11,7 +11,7 @@ import java.util.List;
 public class Location extends Auditable
 {
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long locationid;
 
     @Column(nullable = false, unique = true)
--- co-make-db/src/main/java/com/comake/server/services/UserServiceImpl.java
+++ co-make-db/src/main/java/com/comake/server/services/UserServiceImpl.java
@@ -1,5 +1,21 @@
 package com.comake.server.services;
 
+
+import com.comake.server.models.User;
+import com.comake.server.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service(value = "userService")
 public class UserServiceImpl implements UserService
 {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public User save(User user)
+    {
+        User newUser = userRepository.save(user);
+        return newUser;
+    }
 }
