f7dde2f7d31364552da1a6eda75e81d07e6eb716
seth-bradshaw <seth.bradshaw27@gmail.com>
2021-01-04 19:32:11 -0700

    added comment endpoint and users posts endpoint

--- co-make-db/src/main/java/com/comake/server/services/CommentServiceImpl.java
+++ co-make-db/src/main/java/com/comake/server/services/CommentServiceImpl.java
@@ -5,6 +5,8 @@ import com.comake.server.repository.CommentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
+
 @Service(value = "commentService")
 public class CommentServiceImpl implements CommentService
 {
@@ -17,5 +19,14 @@ public class CommentServiceImpl implements CommentService
         Comment newComment = commentRepository.save(comment);
 
         return newComment;
+    }
+
+    @Override
+    public Comment findCommentById(long id)
+    {
+        Comment comment;
+        comment = commentRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Comment with id " + id + " not found."));
+
+        return comment;
     }
 }
