f138c48c2dc5434780375f52582c7452dc0e99e9
david chang <bjchang@Davids-MacBook-Air.local>
2021-01-07 13:17:51 -0800

    added a GET method userinfo

--- co-make-db/src/main/java/com/comake/server/controllers/UserController.java
+++ co-make-db/src/main/java/com/comake/server/controllers/UserController.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -104,5 +105,14 @@ public class UserController
     {
         userService.delete(id);
         return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/userinfo",
+            produces = {"application/json"})
+    public ResponseEntity<?> getCurrentUserInfo(Authentication authentication)
+    {
+        User u = userService.findByUsername(authentication.getName());
+        return new ResponseEntity<>(u,
+                HttpStatus.OK);
     }
 }
