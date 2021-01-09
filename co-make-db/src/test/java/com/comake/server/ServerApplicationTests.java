62a8467c034de75897b8bcaa41e71efec7932ac5
seth-bradshaw <seth.bradshaw27@gmail.com>
2021-01-08 17:55:42 -0700

    finished post and comment service testing

--- co-make-db/src/test/java/com/comake/server/ServerApplicationTests.java
+++ co-make-db/src/test/java/com/comake/server/ServerApplicationTests.java
@@ -11,7 +11,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @EnableWebMvc
 @SpringBootTest
-class ServerApplicationTests
+public class ServerApplicationTests
 {
     private static final Logger logger = LoggerFactory.getLogger(com.comake.server.ServerApplicationTests.class);
 
